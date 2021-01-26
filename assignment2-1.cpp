807688db453b52e910df6ba4348e348ae389ae4a
Kyuwoong Lee <kwooler@gmail.com>
2021-01-26 22:04:30 +0000

    Commit by Kyu

--- assignment2-1.cpp
+++ assignment2-1.cpp
@@ -1,1 +1,18 @@
 // make your program here
+
+// To submit your assignment,
+// Write your program here and then commit it to be shown in Github
+#include <iostream>
+using namespace std;
+int main ()
+{    
+	int checking;    
+	unsigned int miles;    
+	long days;    
+	checking = -20;    
+	miles = -4276;    
+	days = 18900;    
+	cout << "We have made a long journey of " << miles;   
+	cout << " miles.\n";    
+	cout << "Our checking account balance is " <<  checking;    cout << " \n About " << days << " days ago Columbus";    return 0;
+}
