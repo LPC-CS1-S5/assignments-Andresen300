768633460e86b95cbadd70f2260152a32cc05416
Victoria Andresen <vnandresen@gmail.com>
2021-01-27 22:47:29 +0000

    completed

--- pr2-21.cpp
+++ pr2-21.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main()
-{cout << "Hello    World\n";
+{
+    double reqular_wages, overtime_wages,work_hours=50;
+    double base_pay_rate = 18.25, overtime_pay_rate=27.78, reqular_hours=40, overtime_hours;
+    double total_wages;
+
+    
+    reqular_wages = reqular_hours * base_pay_rate;
+    overtime_hours= work_hours - reqular_hours;
+    overtime_wages = overtime_hours * overtime_pay_rate;
+    total_wages = reqular_wages+ overtime_wages;
+
+    cout << "Regular wages " << reqular_wages << endl;
+    cout << "Overtime wages " << overtime_wages<< endl;
+    cout << "Total wages " << total_wages<< endl;
+    return 0;
 }
--- pr2-21a.cpp
+++
@@ -1,2 +0,0 @@
-//This is the file created in GitHub repository
-//this is the teacher comment i added slashes
