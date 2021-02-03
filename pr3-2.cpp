c4e0d09b5e3cb92b9716e255424b49c842c52a77
Victoria Andresen <vnandresen@gmail.com>
2021-02-03 22:30:08 +0000

    change1 to pr3-2

--- pr3-2.cpp
+++ pr3-2.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
-#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  float males, females, total_students; 
+  float percent_females, percent_males;
+
+  cout << "How many females are in the class?\n";
+  cin >> females;
+  cout << "How many males are in the class?\n";
+  cin >> males;
+
+  total_students = females + males;
+  percent_females = (females/total_students)*100;
+  percent_males = (males/total_students)*100;
+
+  cout << "There are " << percent_males << "% males and " << percent_females << "% females.";
+
+}
