3ae33a48fd76d9d6781f75a664112a1fd6c18849
Victoria Andresen <vnandresen@gmail.com>
2021-03-10 18:00:02 +0000

    done 2

--- Quiz5Q3.cpp
+++ Quiz5Q3.cpp
@@ -10,7 +10,7 @@ int main()
   cout << "Enter the base and the exponent 0 < N < 10,  1 <= M <= 10" << endl;
   cin >> N >> M;
 
-  while (N <= 0 && N >= 10 && M <1 && M >10)
+  while (N <= 0 || N >= 10 && M <1 || M >10)
   {
     cout<< "Enter base and exponent again. 0 < N < 10,  1 <= M <= 10 " << endl;
     cin >> N >> M;
---
+++ Quiz5Q5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int prime, start, finish;
+  cout << "Enter tow numbers and i will tell you all the prime numbers\n";
+  cin >> start >> finish;
+
+  while ( )
+}
