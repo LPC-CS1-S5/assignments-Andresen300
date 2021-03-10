fa8afeebd4f79a6ed94071a548f75b0bc777b3e8
Victoria Andresen <vnandresen@gmail.com>
2021-03-10 23:01:51 +0000

    it works!!!

--- Quiz5Q5.cpp
+++ Quiz5Q5.cpp
@@ -3,37 +3,28 @@ using namespace std;
 
 int main()
 {
-  int prime, start, finish;
-  cout << "Enter tow numbers and i will tell you all the prime numbers\n";
+  int num, i, start, finish;
+  cout << "Enter two numbers and I will tell you all the prime numbers\n";
   cin >> start >> finish;
 
-  while (start < 1 || start > 100 && finish <=2 || finish >=100 && start > finish)
+  while ((start < 1 || start > 100) || (finish <2 || finish >100)|| start > finish)
   {
-    cout << " error \n";
-    cout << "Enter tow numbers and i will tell you all the prime numbers\n";
+    cout << "error \n";
+    cout << "Enter two numbers and I will tell you all the prime numbers\n";
     cin >> start >> finish;
   }
-    if (start == 1)
-    cout << "2"<< " ";
-      for (prime = 3; prime < finish; prime++)
-      {
-        if ( prime % 2 !=0)
-        { 
-          if (prime % 3 !=0)
-          
-            if (prime % 5 !=0)
 
-              if (prime % 7 !=0)
-                cout << prime << " ";
+        for (num = start; num <= finish; num++)
+        { // num : from start to finish
+            // num is prime or not, num = 6;
+            for( i=2; i < num; i++) // 2,3,4,5 go through the loop break and go to the first for loop when condition is true.
+        {
+                if (num % i == 0)
+                    break;
+            }
+            if ( i == num ) //we got to the break after full  iteration for the 2nd for loop
+                cout << num << " ";
         }
-      }
 
-	//   To check 10 is prime,
-	// we need a loop from 2 ~ 9
-	// to check modulus 10, e.g, 10 % 2, 10 % 3, ... 10 % 9
-	// num = 10;
-	// for( i=2; i<num; i++)
-	//.    if( num % i == 0)
-	//          do something
+}
 
-i don't understand 
