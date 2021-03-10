5b8618dd1ed51a6ac61f11ea75278bfc0826c2d8
Victoria Andresen <vnandresen@gmail.com>
2021-03-10 17:50:16 +0000

    done

--- Quiz5Q3.cpp
+++ Quiz5Q3.cpp
@@ -5,18 +5,29 @@ int main()
 {
   int N; // to hold the base
   int M; // the highest pow to rase base 
+  int i=0, powernum;
 
   cout << "Enter the base and the exponent 0 < N < 10,  1 <= M <= 10" << endl;
   cin >> N >> M;
 
   while (N <= 0 && N >= 10 && M <1 && M >10)
-  cout<< "Enter base and exponent again. 0 < N < 10,  1 <= M <= 10 " << endl;
-  cin >> N >> M;
-  
-  for (int i=0; i < M; i++)
   {
-    N=N*i;
-    cout << N << " ";
+    cout<< "Enter base and exponent again. 0 < N < 10,  1 <= M <= 10 " << endl;
+    cin >> N >> M;
+  }
+
+  for (i=0; i <= M; i++)
+  {
+    if (i ==0)
+    {
+      N = 1;
+      cout << N << " ";
+    }
+    if (i >0)
+      {
+        N = N*2;
+        cout << N << " ";
+      }
   }
 
 }
