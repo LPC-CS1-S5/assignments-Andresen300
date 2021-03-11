b932d6dff8a432309a34c489559958614f8506c7
Victoria Andresen <vnandresen@gmail.com>
2021-03-11 02:37:28 +0000

    1st attempt

---
+++ Quiz5Q7r.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+  ifstream inputf;
+  int EmployeeID, num, i ; // to hold for how many employee to process.
+  string EmployeeName, DepartmentName;
+  double Salary, average, counter, totalSalary=0;
+  //Open the file
+  inputf.open("employee.txt");
+  //if the file successfully opened, procces it.
+  if (inputf)
+  {
+    // Read  and display data
+    inputf >>num 
+    cout <<num << endl;
+    for (i=0; i<num;i++)
+    {
+      inputf>>EmployeeID >>EmployeeName >> DepartmentName >>Salary;
+      cout <<EmployeeID << " "<<EmployeeName <<" "<< DepartmentName<<" "<<Salary << endl;
+      totalSalary+=Salary;
+      counter+=1;
+    }
+
+  }
+  average = totalSalary/counter;
+  cout << "Total "<<totalSalary<< "   Average "<<average << endl;
+}
