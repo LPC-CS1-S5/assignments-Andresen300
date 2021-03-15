8fdfcdc1002e8416acce3a286043d754aa92a838
Victoria Andresen <vnandresen@gmail.com>
2021-03-15 23:17:06 +0000

    done

---
+++ ch5/assn5-5lab5-16-1.cpp
@@ -0,0 +1,35 @@
+//Writing the student information
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+  int N;
+  string stuName;
+  double score1, score2;
+
+  cout << "Enter the number of students:";
+  cin >> N;
+
+  ofstream ofs;
+  ofs.open("stuRecord.txt");
+  if (!ofs)
+  {
+    cout << "Open Error\n";
+    exit(0);
+  }
+
+  for (int count=0; count <N; count++)
+  {
+    cout << "Entere student name and two scores\n";
+    cin >> stuName;
+    cin >> score1 >> score2;
+    ofs << stuName << endl;
+    ofs << score1 <<endl;
+    ofs << score2 << endl;
+    
+  }
+  ofs.close();
+}
