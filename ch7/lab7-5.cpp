893274448027163043fcc654901fbeae10dab34e
Victoria Andresen <>
2021-04-21 14:40:16 +0000

    doesn't work

---
+++ ch7/lab7-5.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void shiftleft(int[],int, int);
+void shiftleftone(int[], int);
+void printout(int[],int);
+
+
+const int SIZE=10;
+
+int main()
+{
+
+    int shiftcount;
+    int num[SIZE] ={0,1,2,3,4,5,6,7,8,9};
+
+    cout << "Enter your number for circular shift left\n";
+    cin >> shiftcount;
+
+    printout (num, SIZE);
+    shiftleft ( num, SIZE, shiftcount);
+    printout (num, SIZE);
+}
+
+void shiftleft (int num[], int shifcount, int SIZE)
+{
+    for (int i=0; i < shifcount; i++)
+        shiftleftone(num, SIZE);
+}
+void shiftleftone (int num[], int size)
+{
+    int tmp = num[0];
+    for (int i=0; i<SIZE-1; i++)
+    {
+        num[i]=num[i+1];
+    }
+    num[SIZE-1]= tmp;
+}
+void printout(int numbers[], int size)
+{
+    for(int i = 0; i < SIZE; i++)
+    {
+        cout << setw(3) << numbers[i];
+    }
+    cout << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
