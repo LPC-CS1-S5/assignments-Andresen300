9b1bd0794754883545972c9e8ac4373ac56532e1
Victoria Andresen <>
2021-05-04 22:21:26 +0000

    1

---
+++ Quiz7/Q7-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//function prototype
+int findMax(int[], int, int);
+
+const int N = 10;
+
+int main()
+{
+  int result;
+  int  number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+  result=findMax(number, from, to)
+  cout << "The greatest number in a first half of an array = " << result << endl;
+}
+int findMax ()
