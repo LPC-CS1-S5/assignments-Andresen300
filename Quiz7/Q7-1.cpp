a9c5d1579732cc61347e06ea6a6db03ad1357b51
Victoria Andresen <>
2021-05-04 20:10:52 +0000

    4

--- Quiz7/Q7-1.cpp
+++ Quiz7/Q7-1.cpp
@@ -4,32 +4,40 @@
 using namespace std;
 
 //function prototype
-void findValue(int[], int);
+void findValue(double[], int);
 const int N = 10;
 
 int main()
 {
-  int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+  double number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
   {
-    cout << " The number whos adjecent difference is less than 10: \n";
+    cout << " The numbers whith adjecent difference is less than 10: \n";
     findValue(number, N);
   }
 }
-// function defenition 
+// function defenition
 /* task:    Find the element where the difference between adjacent elements is less
              than 10 .  print.
   Data in:  accepts an array of ints and size.
   Data out:  none.*/
-  void findValue (int number[], int n)
+  void findValue (double number[], int n)
   {
+    double difference=0;
+    double difference2=0;
+
     for(int i = 0; i<N; i++)
     {
-      if (number[i] > 0 && number[i]< 9)
+      difference=0;
+      difference2=0;
+      if (i > 0 && i< 9)
       {
-        if ((abs(number[i] - number[i-1])) < 10) && ((abs(number[i] - number[i+1])) < 10)
-        {
-          cout << number[i];
-        }
-      } 
+        difference = (abs(number[i] - number[i-1]));
+        difference2 =(abs(number[i] - number[i+1]));
+
+        if ( difference < 10 && difference2 <10)
+          {
+              cout << number[i]<< endl;
+          }
+      }
     }
-  }  
+  }
