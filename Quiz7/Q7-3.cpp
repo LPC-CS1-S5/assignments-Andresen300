003cf5467b5ba4fbf5f7d49808de6f702307fe0d
Victoria Andresen <>
2021-05-04 22:51:00 +0000

    3

--- Quiz7/Q7-3.cpp
+++ Quiz7/Q7-3.cpp
@@ -8,12 +8,22 @@ const int N = 10;
 
 int main()
 {
-  int result;
+  int max;
   int  number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
-  result=findMax(number, from, to)
-  cout << "The greatest number in a first half of an array = " << result << endl;
+  int from = 0;
+  int to = N/2;
+  max =findMax(number, from, to);
+  cout << "The greatest number in a first half of an array = " << max << endl;
 }
 int findMax (number[], int from, int to)
 {
-  
+  int max = number[from];
+  for (i= from; i < to; i++)
+  {
+    if (max < number[i])
+    {
+      max = number[i];
+    }
+  }
+  return max;
 }
