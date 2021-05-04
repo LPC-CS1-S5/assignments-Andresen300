19a01b3c5203d0510b23505476bcb99a69190a5b
Victoria Andresen <>
2021-05-04 16:08:48 +0000

    1

---
+++ Quiz7/Q7-1.cpp
@@ -0,0 +1,20 @@
+// Find the element where the difference between adjacent elements is less
+// than 10
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+//function prototype
+
+const int N = 10;
+
+int main()
+{
+  int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+  
+  for(int i = 0; i<10; i++)
+  {
+    
+  }
+}
