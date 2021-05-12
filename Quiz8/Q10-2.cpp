a0ddec2f61649f5afbcdf602c20aa374fc69ffb8
Victoria Andresen <>
2021-05-12 03:00:03 +0000

    1

---
+++ Quiz8/Q10-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+bool idcheck(char[]);
+bool domaincheck (char[]);
+
+int main()
+{
+  char email[20];
+  cout << "Enter your email\n";
+  cin >> email;
+
+  if (idcheck(email))
+  cout << "ID check passed\n";
+  if (domaincheck(email))
+  cout << "Domain check passed\n";
+}
+
+boll idcheck ( char email[])
