87c93143f0f9188d4f380f00436ccbe0fb6e2403
Victoria Andresen <>
2021-05-18 22:46:31 +0000

    3

--- Quiz9/Q1.cpp
+++ Quiz9/Q1.cpp
@@ -1,15 +1,11 @@
-/*We are going to declare the structure for the Employee record. The record contains a couple of personal information for an employee such as
 
-ID: 6 digits numeric value 
-Name: First and last name. ( Two words)
-Salary: Annual salary (0 ~ 500000)
-Department name: The department that the employee works for ( one word)
-Date: The date which the employee starts to work in this company(Mon Day Year)
-The data type for each member data of structure can be declared with the built-in data type or user-defined another structure. The design of the structure depends on the programmer.*/
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
+
+void constructArray (EmpRecords &)
 
 struct EmpRecords {int id;
                    string name;
@@ -19,4 +15,10 @@ struct EmpRecords {int id;
                    int day;
                    int year;};
 
+int main()
+{
+  ifstream ifs;
+  ifs.open("employee.txt");
+  EmpRecords emp[6];  //Array of struct
 
+}
