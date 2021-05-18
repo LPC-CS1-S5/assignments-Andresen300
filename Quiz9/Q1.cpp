5968e639b313f44b6d9d33b8db95a43ecd48e2e3
Victoria Andresen <>
2021-05-18 01:56:30 +0000

    done

---
+++ Quiz9/Q1.cpp
@@ -0,0 +1,21 @@
+/*We are going to declare the structure for the Employee record. The record contains a couple of personal information for an employee such as
+
+ID: 6 digits numeric value 
+Name: First and last name. ( Two words)
+Salary: Annual salary (0 ~ 500000)
+Department name: The department that the employee works for ( one word)
+Date: The date which the employee starts to work in this company(Mon Day Year)
+The data type for each member data of structure can be declared with the built-in data type or user-defined another structure. The design of the structure depends on the programmer.*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+struct Date { int month;
+              int day;
+              int year;};
+
+struct EmpRecords {int id;
+                   string name;
+                   int salary;
+                   string depName;
+                   Date startDay; };
