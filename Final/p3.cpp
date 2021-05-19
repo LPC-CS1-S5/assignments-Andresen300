3c0af0dcc91ef4d237723b2a842d76db3bf302fc
Victoria Andresen <>
2021-05-19 23:58:02 +0000

    2

--- Final/p3.cpp
+++ Final/p3.cpp
@@ -8,15 +8,19 @@ int main()
     const int SIZE1=5;
     const int SIZE2=10;
     
-    int numbers1[2,3,5,9,10};
-    int numbers2{1,2,3,5,9,10,11,13,15,1};
+    int numbers1[]={2,3,5,9,10};
+    int numbers2[]={1,2,3,5,9,10,11,13,15,1};
     
   // look for element of firs array in the second array. travel through element one by one. 
-    for ( int i =0; i<SIZE1; i++)
+    for ( int i =0; i<SIZE2; i++)
     {
-        for(int j =0; j<SIZE2; j++)
+        for(int j =0; j<SIZE1; j++)
         {
-            
+            if (numbers1[j]== numbers2[j])
+            {
+              
+            }
+          
         }
     }
 }
