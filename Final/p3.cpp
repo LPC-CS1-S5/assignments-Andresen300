ffb1dfa6e1d8d37c0ba8b8039cef211bb5ef56d4
Victoria Andresen <>
2021-05-19 23:48:34 +0000

    1

---
+++ Final/p3.cpp
@@ -0,0 +1,22 @@
+#include <iostrea>
+
+
+using namespace std;
+
+int main()
+{
+    const int SIZE1=5;
+    const int SIZE2=10;
+    
+    int numbers1[2,3,5,9,10};
+    int numbers2{1,2,3,5,9,10,11,13,15,1};
+    
+  // look for element of firs array in the second array. travel through element one by one. 
+    for ( int i =0; i<SIZE1; i++)
+    {
+        for(int j =0; j<SIZE2; j++)
+        {
+            
+        }
+    }
+}
